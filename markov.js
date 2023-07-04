7f4f871b3610630b4be852f756d73793621a20b3
Jonathan Hollingsworth <jonathanhollingsworth07@gmail.com>
2023-07-04 15:04:04 -0400

    Initialized

---
+++ markov.js
@@ -0,0 +1,47 @@
+/** Textual markov chain generator */
+
+
+class MarkovMachine {
+
+  /** build markov machine; read in text.*/
+
+  constructor(text) {
+    let words = text.split(/[ \r\n]+/);
+    this.words = words.filter(c => c !== "");
+    this.makeChains();
+  }
+
+  /** set markov chains:
+   *
+   *  for text of "the cat in the hat", chains will be
+   *  {"the": ["cat", "hat"], "cat": ["in"], "in": ["the"], "hat": [null]} */
+
+  makeChains() {
+    this.chains = {}
+    for (let i = 0; i < this.words.length; i++) {
+      const word = this.words[i];
+      if (i === this.words.length - 1) {
+        if (this.chains[word]) {
+          this.chains[word].push(null)
+        } else {
+          this.chains[word] = [null]
+        }
+      } else {
+        if (this.chains[word]) {
+          this.chains[word].push(this.words[i + 1])
+        } else {
+          this.chains[word] = [this.words[i + 1]]
+        }
+      }
+      
+    }
+    return this.chains
+  }
+
+
+  /** return random text from chains */
+
+  makeText(numWords = 100) {
+    // TODO
+  }
+}
